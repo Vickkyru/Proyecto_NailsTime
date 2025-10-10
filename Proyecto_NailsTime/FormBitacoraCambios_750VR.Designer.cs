9de6deb0010e47d61ebdd6a7a8ae54e0404af5ba
vickkyru <mavrusso16@gmail.com>
2025-10-10 13:35:44 -0300

    pepe

---
+++ Proyecto_NailsTime/FormBitacoraCambios_750VR.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Proyecto_NailsTime
+{
+    partial class FormBitacoraCambios_750VR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btnimp = new System.Windows.Forms.Button();
+            this.btnapli = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnom = new System.Windows.Forms.TextBox();
+            this.cmblog = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlimp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(684, 292);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 43;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // btnimp
+            // 
+            this.btnimp.Location = new System.Drawing.Point(684, 333);
+            this.btnimp.Name = "btnimp";
+            this.btnimp.Size = new System.Drawing.Size(75, 23);
+            this.btnimp.TabIndex = 42;
+            this.btnimp.Text = "Activar";
+            this.btnimp.UseVisualStyleBackColor = true;
+            // 
+            // btnapli
+            // 
+            this.btnapli.Location = new System.Drawing.Point(599, 292);
+            this.btnapli.Name = "btnapli";
+            this.btnapli.Size = new System.Drawing.Size(75, 23);
+            this.btnapli.TabIndex = 41;
+            this.btnapli.Text = "Aplicar";
+            this.btnapli.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(338, 296);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 40;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(338, 320);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 39;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(268, 326);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "Fecha fin";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(268, 304);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 13);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Fecha inicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Cod insumo";
+            // 
+            // txtnom
+            // 
+            this.txtnom.Location = new System.Drawing.Point(115, 326);
+            this.txtnom.Name = "txtnom";
+            this.txtnom.Size = new System.Drawing.Size(121, 20);
+            this.txtnom.TabIndex = 27;
+            // 
+            // cmblog
+            // 
+            this.cmblog.FormattingEnabled = true;
+            this.cmblog.Location = new System.Drawing.Point(115, 295);
+            this.cmblog.Name = "cmblog";
+            this.cmblog.Size = new System.Drawing.Size(121, 21);
+            this.cmblog.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Nombre";
+            // 
+            // btnlimp
+            // 
+            this.btnlimp.Location = new System.Drawing.Point(599, 333);
+            this.btnlimp.Name = "btnlimp";
+            this.btnlimp.Size = new System.Drawing.Size(75, 23);
+            this.btnlimp.TabIndex = 24;
+            this.btnlimp.Text = "Limpiar";
+            this.btnlimp.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 31);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Bitacora de Cambios";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(718, 162);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // FormBitacoraCambios_750VR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(794, 379);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnimp);
+            this.Controls.Add(this.btnapli);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtnom);
+            this.Controls.Add(this.cmblog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnlimp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormBitacoraCambios_750VR";
+            this.Text = "FormBitacoraCambios_750VR";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnimp;
+        private System.Windows.Forms.Button btnapli;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnom;
+        private System.Windows.Forms.ComboBox cmblog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlimp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
